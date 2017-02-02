89ee19fee66eccf7195f61dbade64b7549133199
Vitaly Kravtsov <in4lio@gmail.com>
2017-02-02 17:35:58 +0300

    cleanup

--- build.py
+++ build.py
@@ -27,7 +27,7 @@ copylist = (
     ( 'emdr1986x-std-per-lib/README.md'                   , 'Libraries/' ),
     ( 'emdr1986x-std-per-lib/Config/'                     , 'Config/' ),
 
-    ( 'diff/emdr1986x-std-per-lib/'                       , 'Libraries/' ),
+#    ( 'diff/emdr1986x-std-per-lib/'                       , 'Libraries/' ),
 
     ( 'emdr1986x-std-per-lib-doc/Example_Projects/'       , 'Example_Projects/' ),
     ( 'emdr1986x-std-per-lib-doc/Examples/'               , 'Examples/' ),
